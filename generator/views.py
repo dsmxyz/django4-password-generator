ce6f4c0a7b310b749ded65218326f53e9b775ca3
dsmxyz <d.majarali@live.com>
2022-02-01 00:57:43 -0500

    added comments

--- generator/views.py
+++ generator/views.py
@@ -1,24 +1,36 @@
+# Create your views here
+
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
-# Create your views here.
+# Home page
 def home(request):
     return render(request, 'generator/home.html')
 
+# Password generation page/function
 def password(request):
+    # Intialize password variable
     thepassword=''
+    # Add whole alphabet (lowercase) to characters list 
     characters=list('abcdefghijklmnopqrstuvwxyz')
+    # If uppercase is toggled on form, uppercase alphabet will be added to characters list
     if request.GET.get('uppercase'):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-    if request.GET.get('special'):
+    # If special characters are toggles on form, special characters will be added to characters list
         characters.extend(list('!@#$%^&*()-_=+?.>,<'))
+    # If numbers is toggled on form, numbers will be added to characters list
     if request.GET.get('numbers'):
         characters.extend(list('0123456789'))
+    # Sets the length of the generated password based on form input
     length=int(request.GET.get('length'))
+    # For loop that stop when i is greater than the length variable
     for i in range(length):
+        # Each pass of the loop will choose a random character from the character list and place it in the password variable
         thepassword+=random.choice(characters)
+    # Renders html page
     return render(request, 'generator/password.html', {'password': thepassword})
 
+# About us page
 def about(request):
     return render(request, 'generator/about.html')
--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR_SECRET_KEY'
+SECRET_KEY = 'django-insecure-93!rv=h+&vqao&ux&2t-60y=k-aka@zhc-hr7xnra^+!9g1zh6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add password generator app
     'generator',
 ]
 
